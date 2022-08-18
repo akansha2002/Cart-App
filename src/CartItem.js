9fb64ad24da4df270dccf75da3c9c2aceca40969
Akansha Aggarwal <akanshaaggarwal9876@gmail.com>
2022-08-18 13:21:01 +0530

    initial commit

---
+++ src/CartItem.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class CartItem extends React.Component{
+    render() {
+        return(
+        <div className="cart-item">
+            <div className="left-block">
+                <img style={styles.image} alt=""/>
+            </div>
+            <div className="right-block">
+                <div style={{fontSize: 25}}>Phone</div>
+                <div style={{color: '#777'}}>Rs 999</div>
+                <div style={{color: '#777'}}>Qty: 1</div>
+                <div className="cart-item-actions">
+                    {/* Buttons */}
+                </div>
+
+            </div>
+        </div>
+        );
+    }
+}
+
+// camel case in js
+// object created
+// I can apply style using objects
+const styles={
+    image: {
+        height:110,
+        width:110,
+        borderRadius: 4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem
